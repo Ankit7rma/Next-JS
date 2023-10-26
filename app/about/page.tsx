ed933179c34e30764887226411980e18a12f9e10
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-26 10:28:02 +0530

    loading + error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function About() {
+  // throw new Error("Not Today!")
   return (
     <div>
         <h1 className='m-5 p-5'>About</h1>
