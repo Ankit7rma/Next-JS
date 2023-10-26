7fe71d5595f0a979bb521f62b1571306a94a9e6d
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-26 09:57:09 +0530

    Pages Navigation

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1 className='m-5 p-5'>About</h1>
+        <Link href="/">Go to Home Page</Link>      
+    </div>
+  )
+}
