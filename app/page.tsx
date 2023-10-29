890a98130b104cade4e2c7025a79f0b83e3fcd74
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-29 10:42:22 +0530

    Client Component Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
-
+import { Inter } from 'next/font/google'
+const inter = Inter({subsets:['latin']})
 export default function Home() {
   return (
     <main className="flex  flex-col items-center justify-between p-24">
         Hello World
         <Link href="about">Go To About Page</Link>
         <Link href="users">Go to Users</Link>
+        <Link href="client_components">Client-Components</Link>
     </main>
   )
 }
